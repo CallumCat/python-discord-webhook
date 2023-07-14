a8edb5685a2b6fb017af43e2ddd04e788206de10
Callum <40074095+CallumCat@users.noreply.github.com>
2023-07-13 20:00:15 -0400

    aaaa

--- examples/basic_webhook.py
+++
@@ -1,4 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-webhook = DiscordWebhook(url="your webhook url", content="Webhook Message")
-response = webhook.execute()
--- examples/manage_rate_limits.py
+++
@@ -1,8 +0,0 @@
-from discord_webhook import DiscordWebhook
-
-webhook = DiscordWebhook(
-    url="your webhook url",
-    rate_limit_retry=True,
-    content="Webhook Message",
-)
-response = webhook.execute()
